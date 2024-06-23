5f75c2806e42dcf93e64aba96584052426581990
harris faqih <qifahsirrah@gmail.com>
2024-06-23 13:45:22 +0700

    adding add product

--- dashboard/src/router/routes/adminRoutes.js
+++ dashboard/src/router/routes/adminRoutes.js
@@ -4,6 +4,7 @@ import { lazy } from "react";
 const AdminDashboard = lazy(() => import("../../views/admin/AdminDashboard"));
 const Orders = lazy(() => import("../../views/admin/Orders"));
 const Category = lazy(() => import("../../views/admin/Category"));
+const AddProduct = lazy(() => import("../../views/admin/AddProduct"));
 const Sellers = lazy(() => import("../../views/admin/Sellers"));
 const PaymentRequest = lazy(() => import("../../views/admin/PaymentRequest"));
 const DeactiveSellers = lazy(() =>
@@ -29,6 +30,11 @@ export const adminRoutes = [
     path: "admin/dashboard/category",
     element: <Category />,
     role: "admin",
+  },
+  {
+    path: "admin/dashboard/add-product",
+    element: <AddProduct />,
+    ability: "admin",
   },
   {
     path: "admin/dashboard/sellers",
